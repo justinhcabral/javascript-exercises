88a8ec8476e0de6879d6fe457e9f94a266b0b8fd
Justin Cabral <70143234+justinhcabral@users.noreply.github.com>
2024-04-19 00:00:03 +0800

    Solve problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, numberOfTimes) {
+    let string ="";
+    if(numberOfTimes<0){
+        return "ERROR";
+    }
+    for(i=0; i<numberOfTimes;i++){
+        string+=text;
+    }
+    return string;
 };
 
 // Do not edit below this line
