1621383db53c3c51d19a76821f7022c654d047cc
Justin Cabral <70143234+justinhcabral@users.noreply.github.com>
2024-04-20 02:43:00 +0800

    Solve problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...numbersToRemove) {
+    let newArray = [];
+    for(const element of array){
+        if(!numbersToRemove.includes(element)){
+            newArray.push(element);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
