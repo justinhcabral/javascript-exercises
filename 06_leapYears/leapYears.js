dcd0b16839a1592c105599aa6ed27931dc131c68
Justin Cabral <70143234+justinhcabral@users.noreply.github.com>
2024-04-21 03:18:05 +0800

    Solve problem 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(yearInQuestion) {
+    if(yearInQuestion%4==0 && (yearInQuestion%100!=0 ||yearInQuestion%400==0)){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
