0a0009b7743c75929979ff5c3a1ed536b8401035
Justin Cabral <70143234+justinhcabral@users.noreply.github.com>
2024-04-20 17:58:06 +0800

    Finish problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    let sum = 0;
+    if(start<0||end <0){
+        return "ERROR";
+    } else if(typeof start != 'number' || typeof end != 'number'){
+        return "ERROR";
+    } else if(start<end){
+        for(let i=start;i<=end;i++){
+            sum += i;
+        }
+        return sum;
+    } else if(start>end){
+        for(let i=start;i>=end;i--){
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
